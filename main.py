8489b3747e886fd7a4fee89e40f8777c97d61086
Yassi <yasaman.khorsandian@tufts.edu>
2019-04-16 15:23:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-"""This is the python file that your instructors will run to test your code
-make sure it runs correctly when someone downloads your repository. You 
-might want to test this on a classmates computer to be sure it works!""
+"""
+Authors: @YassiKhorsandian & @Allison Choi
+""
 
-# This files should not contain any function defitions
+# This files should not contain any function definitions
 
 
 # IMPORT STATEMENTS
