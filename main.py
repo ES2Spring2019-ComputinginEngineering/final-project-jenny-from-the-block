24272e485df9023a8ae52719806891ec7a9927ee
Yassi <yasaman.khorsandian@tufts.edu>
2019-04-23 15:57:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ picture2 = Image.open("SunScaled.png")
 sunpixeldata = getPixelData(picture2, 0, 613, 451)
 
 #Image processing
-convPop = convolveImage(picture)
+convolveImage(picture)
+picture3 = Image.open('convPop.png')
+CONVpoppixeldata = getPixelData(picture3, 1, 613, 451)
 #conPopData = getPixelData(convPop, 1, 451, 613)
 
 #Graphing ratio of sun/population
-graphPixelData(sunpixeldata, poppixeldata)
+graphPixelData(sunpixeldata, CONVpoppixeldata)
