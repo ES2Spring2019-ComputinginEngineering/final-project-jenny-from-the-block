3910ee5117a54c351268e0e3ffafd1a101912f0d
Yassi <yasaman.khorsandian@tufts.edu>
2019-04-23 15:20:57 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,7 +5,6 @@ Created on Tue Apr 16 16:02:14 2019
 
 @author: Yassi Khorsandian & Allison Choi
 """
-from PIL import Image 
 import imageio
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,9 +15,6 @@ def openImage(pic):
     plt.figure()
     plt.imshow(im)
     plt.show()
-  
-openImage("PopulationScaled.png")
-openImage("SunScaled.png")
 
 def getPixelData(pic, RGBA, width, height):
     picData = np.zeros((width, height))
@@ -28,22 +24,14 @@ def getPixelData(pic, RGBA, width, height):
             picData[i,j] = pix[i,j][RGBA]
     return picData
 
-picture = Image.open('PopulationScaled.png') 
-poppixeldata = getPixelData(picture, 1, 613, 451)
-picture2 = Image.open("SunScaled.png")
-sunpixeldata = getPixelData(picture2, 0, 613, 451)
-
 def convolveImage(pixeldata, color):
     kern = np.array([[-1, -1,-1],[0, 0, 0],[1, 1, 1]])
     blur = ndimage.convolve(pixeldata,kern)
     plt.figure()
     plt.imshow(blur, cmap = color) 
     plt.show()
-    
-convolveImage(poppixeldata, "BuGn")
-convolveImage(sunpixeldata, "YlOrRd")
 
-def graphPixelData():
+def graphPixelData(sunpixeldata, poppixeldata):
     width,height = poppixeldata.shape
     ratio = np.zeros((width, height))
     for i in range(width):
@@ -57,7 +45,3 @@ def graphPixelData():
     plt.ylabel("Sunlight to population ratio")
     plt.xlabel("Pixel Index") 
     plt.show()
-    
-graphPixelData()
-
-    
