313655676f9981bd63234dcedee26451138f20c9
Yassi <yasaman.khorsandian@tufts.edu>
2019-04-24 12:19:13 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,16 +6,20 @@ Created on Tue Apr 16 16:02:14 2019
 @author: Yassi Khorsandian & Allison Choi
 """
 import imageio
+from PIL import Image 
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import ImageFilter
+from scipy.misc import toimage
+from PIL import ImageFilter, Image
 
+#Opens the image in form of an array
 def openImage(pic):
     im = np.array(imageio.imread(pic))
     plt.figure()
     plt.imshow(im)
     plt.show()
 
+#Finds the RGBA pixel data of image & returns as an array 
 def getPixelData(pic, RGBA, width, height):
     picData = np.zeros((width, height))
     pix = pic.load()
@@ -24,24 +28,84 @@ def getPixelData(pic, RGBA, width, height):
             picData[i,j] = pix[i,j][RGBA]
     return picData
 
-def convolveImage(picture):  #Convolve each image 
+#Blurs image to reduce noise
+def convolveImage(picture):  
     blurred_image = picture.filter(ImageFilter.BLUR)
     plt.figure()
     plt.imshow(blurred_image) 
     plt.show()
     blurred_image.save("convPop.png")
 
-def graphPixelData(sunpixeldata, poppixeldata):
+#Finds ratio of sunlight/population & returns as an array
+def findRatio(sunpixeldata, poppixeldata):
     width,height = poppixeldata.shape
     ratio = np.zeros((width, height))
     for i in range(width):
         for j in range(height):
-            if poppixeldata[i,j] == 0:
+            if poppixeldata[i,j] < 50:
                 ratio[i,j] = 0
             else:
                 ratio[i,j] = sunpixeldata[i,j]/poppixeldata[i,j]
+    return ratio
+
+#Finds the maximum value of the ratios & the index of the point with maximum value
+def findMaxIndex(ratio):
+    width,height = ratio.shape
+    maximum = np.max(ratio)
+    ind = 0
+    ind2 = 0
+    for i in range(width-1):
+            for j in range(height-1):
+                if ratio[i,j] == maximum:
+                    ind = i
+                    ind2 = j
+    return maximum, ind, ind2    
+
+#Graphs the sunlight & population ratio 
+def graphPixelData1(sunpixeldata, poppixeldata, ratio):
+    width,height = poppixeldata.shape
     plt.figure()
     plt.plot(ratio, "ko")
     plt.ylabel("Sunlight to population ratio")
     plt.xlabel("Pixel Index") 
+    plt.show()
+    
+#Graphs the sunlight & population ratio
+def graphPixelData2(pic, ratio):
+    im = np.array(imageio.imread(pic))
+    width,height = ratio.shape
+    for i in range(width-1):
+        for j in range(height-1):
+            im[j,i,0]=ratio[i,j]*30
+            im[j,i,1]=0
+            im[j,i,2]=0
+    plt.figure()
+    plt.imshow(im)
+    plt.show()
+    
+def placeLocator(pic,ratio):
+    maximum, ind, ind2 = findMaxIndex(ratio)
+    im = np.array(imageio.imread(pic))
+    width,height = ratio.shape
+    for i in range(width-1):
+        for j in range(height-1):
+            im[j,i,0]=ratio[i,j]*30
+            im[j,i,1]=0
+            im[j,i,2]=0
+    p = ind - 8
+    q = ind2 - 8
+    while p < (ind+8):
+        while q < (ind2+8):
+            im[p,q,2] = 200
+            q = q + 1
+        p = p + 1
+    p = ind - 8
+    q = ind2 - 8
+    while q < (ind2+8):
+        while p < (ind+8):
+            im[p,q,2] = 200
+            p = p + 1
+        q = q + 1
+    plt.figure()
+    plt.imshow(im)
     plt.show()
