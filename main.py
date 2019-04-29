637ba339aa89a016d96405a1035f991f778ecc4e
Yassi <yasaman.khorsandian@tufts.edu>
2019-04-29 12:18:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ openImage("SunScaled.png")
 
 #Getting pixel data from image RGBA values
 picture = Image.open('PopulationScaled.png') 
-poppixeldata = getPixelData(picture, 1, 613, 451)
+width,height = picture.size
+poppixeldata = getPixelData(picture, 1, width, height)
 picture2 = Image.open("SunScaled.png")
-sunpixeldata = getPixelData(picture2, 0, 613, 451)
+sunpixeldata = getPixelData(picture2, 0, width, height)
 
 #Image processing
 convolveImage(picture)
 picture3 = Image.open('convPop.png')
-CONVpoppixeldata = getPixelData(picture3, 1, 613, 451)
+CONVpoppixeldata = getPixelData(picture3, 1, width, height)
 #conPopData = getPixelData(convPop, 1, 451, 613)
 
 #Finding ratio of sunlight/population & maximum value with its index
